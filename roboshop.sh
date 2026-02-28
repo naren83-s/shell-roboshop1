2d9fdb3a79f49a3caea43b31af0a894ed1aba2ba
narendra <narendra.sunkara83@gmail.com>
2026-02-28 12:08:28 +0400

    action

---
+++ roboshop.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+SG_ID="sg-06836c5944ba0ff00"
+AMI_ID="ami-0220d79f3f480ecf5"
+HOST_ZONE_ID="Z0354649BHBBW98BVSKE"
+DOMAIN_NAME="naren83.online"
+
+for instance in $@
+do
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+
+echo "Launched Instance ID: $INSTANCE_ID"
+
+  if [ $instance == "frontend" ]; then
+    IP=$(
+        aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME"
+  else
+    IP=$(
+        aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PriateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+  fi
+
+  echo "IP Adress: $IP"
+
+  aws route53 change-resource-record-sets \
+  --hosted-zone-id $HOST_ZONE_ID \
+  --change-batch '
+
+       {
+         "Comment": "Update A record for test.example.com",
+         "Changes": [
+          {
+             "Action": "UPSERT",
+             "ResourceRecordSet": {
+             "Name": "'$RECORD_NAME'",
+             "Type": "A",
+             "TTL": 1,
+             "ResourceRecords": [
+              {
+               "Value": "'$IP'"
+              }
+              ]
+             }
+           }
+           ]
+        }
+        '
+    echo="record updated for $instance
+
+
+
+done
