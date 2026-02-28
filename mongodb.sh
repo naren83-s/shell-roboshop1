95674ffb6bdcf6964248cd5a35cd49f39ff0728a
narendra <narendra.sunkara83@gmail.com>
2026-02-28 12:42:06 +0400

    action

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOG_FOLDER/$0.log"
+LOGS_FOLDER="/var/log/shell-roboshop1"
+LOGS_FILE="/var/log/shell-roboshop1/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
