6d7bc32fe027ee33ccde3a2d8bfeb9a67e6ab79c
narendra <narendra.sunkara83@gmail.com>
2026-03-03 17:42:16 +0400

    action

--- payment.sh
+++ payment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/hell-roboshop1"
-LOGS_FILE="$LOG_FOLDER/$0.log"
+LOGS_FOLDER="/var/log/shell-roboshop1"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -10,59 +10,56 @@ N="\e[0m"
 SCRIPT_DIR=$PWD
 MYSQL_HOST=mysql.naren83.online
 
-
 if [ $USERID -ne 0 ]; then
-   echo -e "$R Please run the script in root user $N" | tee -a $LOGS_FILE
-   exit1
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-       echo -e "$2 ......... $R FAILURE $N" | tee -a $LOGS_FILE
-       exit1
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
     else
-       echo -e "$2 ......... $G SUCESS $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
-dnf install python3 gcc python3-devel -y
-VALIDATE $? "Installing phython"
+dnf install python3 gcc python3-devel -y &>> $LOGS_FILE
+VALIDATE $? "Installing Python"
 
 id roboshop &>> $LOGS_FILE
-    if [ $? -ne 0 ]; then
-        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
-        VALIDATE $? " Creating system user"
-    else
-       echo -e "Roboshop user already exit ... $Y SKIPPING $N" &>> $LOGS_FILE
-    fi
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
 
-mkdir /app 
-VALIDATE $? "Creating new app directory"
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>> $LOGS_FILE
-Validate $? "Downloading payment code"
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>> $LOGS_FILE
+VALIDATE $? "Downloading payment code"
 
 cd /app
 VALIDATE $? "Moving to app directory"
 
 rm -rf /app/*
-VALIDATE $? " Removing existing code"
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/payment.zip &>> $LOGS_FILE
 VALIDATE $? "Uzip payment code"
 
 cd /app 
-pip3 install -r requirements.txt  &>> $LOGS_FILE
-VALIDATE $? "installing depancies"
+pip3 install -r requirements.txt &>> $LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
 cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
-VALIDATE $? "Creating systemctl service"
+VALIDATE $? "Created systemctl service"
 
 systemctl daemon-reload
 systemctl enable payment &>> $LOGS_FILE
 systemctl start payment
 VALIDATE $? "Enabled and started payment"
-
-  
