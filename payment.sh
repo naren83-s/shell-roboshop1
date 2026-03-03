9a84c5173de394b8ffd5b4ec24c4683b6ab849e4
narendra <narendra.sunkara83@gmail.com>
2026-03-03 17:38:29 +0400

    action

--- payment.sh
+++ payment.sh
@@ -19,7 +19,7 @@ fi
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
-    if [$1 -ne 0 ]; then
+    if [ $1 -ne 0 ]; then
        echo -e "$2 ......... $R FAILURE $N" | tee -a $LOGS_FILE
        exit1
     else
@@ -32,16 +32,16 @@ VALIDATE $? "Installing phython"
 
 id roboshop &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
-        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop 
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
         VALIDATE $? " Creating system user"
     else
-       echo -e "Roboshop user already exit ... $Y SKIPPING $N"
+       echo -e "Roboshop user already exit ... $Y SKIPPING $N" &>> $LOGS_FILE
     fi
 
 mkdir /app 
 VALIDATE $? "Creating new app directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOGS_FILE
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>> $LOGS_FILE
 Validate $? "Downloading payment code"
 
 cd /app
@@ -50,18 +50,18 @@ VALIDATE $? "Moving to app directory"
 rm -rf /app/*
 VALIDATE $? " Removing existing code"
 
-unzip /tmp/payment.zip &>>$LOGS_FILE
+unzip /tmp/payment.zip &>> $LOGS_FILE
 VALIDATE $? "Uzip payment code"
 
 cd /app 
-pip3 install -r requirements.txt  &>>$LOGS_FILE
+pip3 install -r requirements.txt  &>> $LOGS_FILE
 VALIDATE $? "installing depancies"
 
 cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 VALIDATE $? "Creating systemctl service"
 
 systemctl daemon-reload
-systemctl enable payment &>>$LOGS_FILE
+systemctl enable payment &>> $LOGS_FILE
 systemctl start payment
 VALIDATE $? "Enabled and started payment"
 
